20ed8eacb986d9b97d8e4a832f0ab92dcb66bbd7
Bidhu1024 <bidhu1024av@gmail.com>
2022-12-18 09:37:26 +0530

    completed upto rapid api call

--- src/components/SearchExcercises.jsx
+++ src/components/SearchExcercises.jsx
@@ -1,47 +1,72 @@
-import React, {useState, useEffect} from 'react'
-import {Box, Button, Stack, TextField, Typography} from '@mui/material'
+import React, { useState, useEffect } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 const SearchExcercises = () => {
-  return (
-   <Stack alignItems='center' m='37px' justifyContent='center' p='20px'>
-<Typography sx={{fontWeight:'700', fontSize:{
-    lg:'44x', xs:'30px'
-}}} mb='50px' textAlign='center'>
-    Awesome Exercises you <br/> should know
-</Typography>
-<Box sx={{position:'relative', mb:'72px'}}>
-<TextField height = '76px' value ="" onChange ={(e)=>{}} 
-placeholder= 'Search'
-type='text'
-sx={{input:{
-    fontWeight:'700',
-    border:'none',
-    borderRadius:'4px'
-},
-width:{
-    lg:'1000px', xs:'350px',
-},
-backgroundColor:'#ffffff',
-borderRadius:'40px'
-}}
-/>
-<Button className='search-btn'
-sx={{bgcolor:'#FF2625',
-color:'#fff',
-textTransform:'none',
-width:{
-    lg:'175px', xs:'80px'
-},
-fontSize:{lg:'20px', xs:'14px'},
-height:'56px',
-position:'absolute',
-right:'0'
-}}
->Search</Button>
-
-</Box>
-   </Stack>
-  )
+    
+  const [search, setSearch] = useState("");
+  const handleSearch = async ()=>{
+if(search){
+    // const excerciseData = await fetchData();
 }
+  }
+  return (
+    <Stack alignItems="center" m="37px" justifyContent="center" p="20px">
+      <Typography
+        sx={{
+          fontWeight: "700",
+          fontSize: {
+            lg: "44x",
+            xs: "30px",
+          },
+        }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises you <br /> should know
+      </Typography>
+      <Box sx={{ position: "relative", mb: "72px" }}>
+        <TextField
+          height="76px"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search"
+          type="text"
+          sx={{
+            input: {
+              fontWeight: "700",
+              border: "none",
+              borderRadius: "4px",
+            },
+            width: {
+              lg: "1000px",
+              xs: "350px",
+            },
+            backgroundColor: "#ffffff",
+            borderRadius: "40px",
+          }}
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: {
+              lg: "175px",
+              xs: "80px",
+            },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
+};
 
-export default SearchExcercises
+export default SearchExcercises;
